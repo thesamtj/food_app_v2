f09479797d818be7fe032620f87471a0221fccfa
Samuel Tijani <omalitijani@gmail.com>
2022-01-27 14:14:33 +0100

    Add OnboardingScreen

--- lib/navigation/app_router.dart
+++ lib/navigation/app_router.dart
@@ -54,6 +54,8 @@ class AppRouter extends RouterDelegate
         if (appStateManager.isInitialized && !appStateManager.isLoggedIn)
           LoginScreen.page(),
         // TODO: Add OnboardingScreen
+        if (appStateManager.isLoggedIn && !appStateManager.isOnboardingComplete)
+          OnboardingScreen.page(),
         // TODO: Add Home
         // TODO: Create new item
         // TODO: Select GroceryItemScreen
