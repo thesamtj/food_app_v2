d9eff70fc2ffb67a8a433e6c857ed4237f4dde80
Samuel Tijani <omalitijani@gmail.com>
2022-01-27 13:46:37 +0100

    Remove

--- lib/main.dart
+++ lib/main.dart
@@ -3,7 +3,6 @@ import 'package:provider/provider.dart';
 
 import 'fooderlich_theme.dart';
 import 'models/models.dart';
-import 'screens/splash_screen.dart';
 // TODO: Import app_router
 import 'navigation/app_router.dart';
 
