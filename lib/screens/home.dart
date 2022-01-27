efb66a57e4e429d8d46c4df58772dcbb26901ea4
Samuel Tijani <omalitijani@gmail.com>
2022-01-27 15:04:50 +0100

    home -> profile

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -94,6 +94,8 @@ class _HomeState extends State<Home> {
         ),
         onTap: () {
           // TODO: home -> profile
+          Provider.of<ProfileManager>(context, listen: false)
+              .tapOnProfile(true);
         },
       ),
     );
