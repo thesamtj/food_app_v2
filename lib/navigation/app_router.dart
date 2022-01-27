249e000f49b4b797c31e554c622e9690a9275143
Samuel Tijani <omalitijani@gmail.com>
2022-01-27 15:16:10 +0100

    Handle state when user closes WebView screen

--- lib/navigation/app_router.dart
+++ lib/navigation/app_router.dart
@@ -122,6 +122,9 @@ class AppRouter extends RouterDelegate
       profileManager.tapOnProfile(false);
     }
     // TODO: Handle state when user closes WebView screen
+    if (route.settings.name == FooderlichPages.raywenderlich) {
+      profileManager.tapOnRaywenderlich(false);
+    }
     // 6
     return true;
   }
