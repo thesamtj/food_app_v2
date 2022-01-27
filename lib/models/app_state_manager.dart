242b63e591bcf299bc2e3948ab6f6b69507e8c86
Samuel Tijani <omalitijani@gmail.com>
2022-01-27 13:11:04 +0100

    Add initializeApp

---
+++ lib/models/app_state_manager.dart
@@ -0,0 +1,45 @@
+import 'dart:async';
+import 'package:flutter/material.dart';
+
+// 1
+class FooderlichTab {
+  static const int explore = 0;
+  static const int recipes = 1;
+  static const int toBuy = 2;
+}
+
+class AppStateManager extends ChangeNotifier {
+  // 2
+  bool _initialized = false;
+  // 3
+  bool _loggedIn = false;
+  // 4
+  bool _onboardingComplete = false;
+  // 5
+  int _selectedTab = FooderlichTab.explore;
+
+  // 6
+  bool get isInitialized => _initialized;
+  bool get isLoggedIn => _loggedIn;
+  bool get isOnboardingComplete => _onboardingComplete;
+  int get getSelectedTab => _selectedTab;
+
+// TODO: Add initializeApp
+  void initializeApp() {
+    // 7
+    Timer(
+      const Duration(milliseconds: 2000),
+      () {
+        // 8
+        _initialized = true;
+        // 9
+        notifyListeners();
+      },
+    );
+  }
+// TODO: Add login
+// TODO: Add completeOnboarding
+// TODO: Add goToTab
+// TODO: Add goToRecipes
+// TODO: Add logout
+}
