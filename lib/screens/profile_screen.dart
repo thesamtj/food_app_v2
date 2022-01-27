74340594a77d3a52fedf9037bf54bf2dd81fe791
Samuel Tijani <omalitijani@gmail.com>
2022-01-27 15:16:57 +0100

    Logout user

--- lib/screens/profile_screen.dart
+++ lib/screens/profile_screen.dart
@@ -69,6 +69,11 @@ class _ProfileScreenState extends State<ProfileScreen> {
           title: const Text('Log out'),
           onTap: () {
             // TODO: Logout user
+            // 1
+            Provider.of<ProfileManager>(context, listen: false)
+                .tapOnProfile(false);
+// 2
+            Provider.of<AppStateManager>(context, listen: false).logout();
           },
         )
       ],
